7d8c002979b1f3f126059769e874cd73c45845ad
李東霖 <lee097019@gmail.com>
2019-03-16 13:31:55 +0000

    Write report and modify code for demo

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -63,29 +63,30 @@ def sigmoid(x):
 def derivative_sigmoid(x):
     return np.multiply(x, 1.0 - x)
 
-def loss(pred_y, y):
-    return np.mean((y - pred_y)**2)
+def loss(y, y_hat):
+    return np.mean((y - y_hat)**2)
     
-def derivative_loss(pred_y, y):
-    return (pred_y - y)*(2/pred_y.shape[0])
+def derivative_loss(y, y_hat):
+    return (y - y_hat)*(2/y.shape[0])
 
 class layer():
-    def __init__(self, i, o):
-        self.w = np.random.normal(0.5, 0.1, (i+1, o))
+    def __init__(self, input_size, output_size):
+        self.w = np.random.normal(0, 1, (input_size+1, output_size))
         
     def forward(self, x):
         x = np.append(x, np.ones((x.shape[0],1)), axis=1)
         self.forward_gradient = x
-        self.z = sigmoid(np.matmul(x, self.w))
-        return self.z
+        self.y = sigmoid(np.matmul(x, self.w))
+        return self.y
     
     def backward(self, derivative_C):
-        self.backward_gradient = np.multiply(derivative_sigmoid(self.z), derivative_C)
+        self.backward_gradient = np.multiply(derivative_sigmoid(self.y), derivative_C)
         return np.matmul(self.backward_gradient, self.w[:-1].T) 
 
     def update(self, learning_rate):
         self.gradient = np.matmul(self.forward_gradient.T, self.backward_gradient)
         self.w -= learning_rate*self.gradient
+        return self.gradient
         
 class NN():
     def __init__(self, sizes, learning_rate = 0.1):
@@ -113,3 +114,50 @@ class NN():
         for l in self.l:
             gradients += [l.update(self.learning_rate)]
         return gradients
+    
+if __name__ == "__main__":
+    nn_linear = NN([2,4,4,1], 1)
+    nn_XOR = NN([2,4,4,1], 1)
+    epoch_count = 10000
+    loss_threshold = 0.005
+    linear_stop = False
+    XOR_stop = False
+    x_linear, y_linear = generate_linear()
+    x_XOR, y_XOR = generate_XOR_easy()
+    for i in range(epoch_count):
+        if not linear_stop:
+            y = nn_linear.forward(x_linear)
+            loss_linear = loss(y, y_linear)
+            nn_linear.backward(derivative_loss(y, y_linear))
+            nn_linear.update()
+            
+            if loss_linear < loss_threshold:
+                print('linear is covergence')
+                linear_stop = True
+        
+        if not XOR_stop:
+            y = nn_XOR.forward(x_XOR)
+            loss_XOR = loss(y, y_XOR)
+            nn_XOR.backward(derivative_loss(y, y_XOR))
+            nn_XOR.update()
+            
+            if loss_XOR < loss_threshold:
+                print('XOR is covergence')
+                XOR_stop = True
+    
+        if i%200 == 0 or (linear_stop and XOR_stop):
+            print(
+                '[{:4d}] linear loss : {:.4f} \t XOR loss : {:.4f}'.format(
+                    i, loss_linear, loss_XOR))
+            
+        if linear_stop and XOR_stop:
+            break
+            
+    y1 = nn_linear.forward(x_linear)
+    show_result(x_linear, y_linear, y1)
+    print('linear test loss : ', loss(y1, y_linear))
+    y2 = nn_XOR.forward(x_XOR)
+    show_result(x_XOR, y_XOR, y2)
+    print('XOR test loss : ', loss(y2, y_XOR))
+    print('\n linear test result : \n',y1)
+    print('\n XOR test result : \n',y2)

c9547094cb1d34a25981c4be6bd8e62cb5f7f3e1
李東霖 <lee097019@gmail.com>
2019-04-13 15:46:55 +0000

    Establish lab3 and Finish dataloader

---
+++ lab3/dataloader.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from torch.utils import data
+from torchvision import transforms
+import numpy as np
+import os
+import PIL
+
+def getData(mode):
+    if mode == 'train':
+        img = pd.read_csv('train_img.csv')
+        label = pd.read_csv('train_label.csv')
+        return np.squeeze(img.values), np.squeeze(label.values)
+    else:
+        img = pd.read_csv('test_img.csv')
+        label = pd.read_csv('test_label.csv')
+        return np.squeeze(img.values), np.squeeze(label.values)
+
+
+class RetinopathyDataset(data.Dataset):
+    def __init__(self, root, mode):
+        """
+        Args:
+            root (string): Root path of the dataset.
+            mode : Indicate procedure status(training or testing)
+
+            self.img_name (string list): String list that store all image names.
+            self.label (int or float list): Numerical list that store all ground truth label values.
+        """
+        self.root = root
+        self.img_name, self.label = getData(mode)
+        self.mode = mode
+        print("> Found %d images..." % (len(self.img_name)))
+
+    def __len__(self):
+        """'return the size of dataset"""
+        return len(self.img_name)
+
+    def __getitem__(self, index):
+        """something you should implement here"""
+
+        """
+           step1. Get the image path from 'self.img_name' and load it.
+                  hint : path = root + self.img_name[index] + '.jpeg'
+           
+           step2. Get the ground truth label from self.label
+                     
+           step3. Transform the .jpeg rgb images during the training phase, such as resizing, random flipping, 
+                  rotation, cropping, normalization etc. But at the beginning, I suggest you follow the hints. 
+                       
+                  In the testing phase, if you have a normalization process during the training phase, you only need 
+                  to normalize the data. 
+                  
+                  hints : Convert the pixel value to [0, 1]
+                          Transpose the image shape from [H, W, C] to [C, H, W]
+                         
+            step4. Return processed image and label
+        """
+        path = os.path.join(self.root, self.img_name[index] + '.jpeg')
+        img = PIL.Image.open(path)
+        img = transforms.ToTensor()(img)
+        
+        label = self.label[index]
+        
+        return img, label
